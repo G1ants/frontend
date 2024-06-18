f84617aa313b82c821e363d3aad5b420ad4d2f5f
jinyangTF <jinyang@tinyfish.io>
2024-06-17 22:55:27 -0700

    Update

--- app/menu/container.py
+++ app/menu/container.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
+if 'agent' not in st.session_state:
+    st.session_state.agent = None
+    
 def display_menu():
     st.button(label="Paul Graham")
     st.button(label="Naval Ravikant")
     st.button(label="Steve Jobs")
+    st.button(label="Napoleon")
+    st.button(label="Oppenheimer")
---
+++ app/models/agent.py
@@ -0,0 +1,8 @@
+from enum import StrEnum
+
+class Agent(StrEnum):
+    PAUL_GRAHAM = "Paul Graham"
+    NAVAL_RAVIKANT = "Naval Ravikant"
+    STEVE_JOBS = "Steve Jobs"
+    NAPOLEON = "Napoleon"
+    OPPENHEIMER = "Oppenheimer"
